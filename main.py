b59c0725b21fa9491ac52b87e9de652660caf501
hasindusithmin <hasindusithmin64@gmail.com>
2021-10-16 08:28:33 +0530

    due

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
 
 from fastapi import FastAPI
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
 
 app = FastAPI()
+sched = BackgroundScheduler()
 
 
 @app.get('/')
 def http():
     return {'message':'hello world'}
+
+def job_function():
+    print('hello world')
+
+sched.add_job(job_function, CronTrigger.from_crontab('*/1 * * * *'))
+sched.start()
