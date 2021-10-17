f179a687105b9df1163c5d8b1d9f3190ab01407e
hasindusithmin <hasindusithmin64@gmail.com>
2021-10-17 11:32:54 +0530

    otto

--- jobs/tele.py
+++ jobs/tele.py
@@ -1,7 +1,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-import os
+from os import environ
 from datetime import datetime
 from dateparser.search import search_dates
 from pytz import timezone    
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 
 
 def sendNews():
-    #download html file
     r = requests.get('http://www.adaderana.lk/hot-news/',headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
     #create instance variable        
     soup = BeautifulSoup(r.content,'lxml')  
@@ -17,21 +16,21 @@ def sendNews():
     #sri lanka timezone
     colombo = timezone('Asia/Colombo')
     now = datetime.now(tz=colombo)
-    #find require news
+    #load enviroment variable
+    load_dotenv()
     for story in stories:
         time = story.find('div',class_='comments pull-right').select('span')[0].text
         entity = search_dates(time)[0][1]    
-        #only take today news
-        if entity.strftime('%d') == now.strftime('%d'):
+        if entity.strftime('%d:%H') == now.strftime('%d:%H'):
             head  = story.select('h2 a')[0].text
             body = story.find('p').text
-            #load enviroment variable
-            load_dotenv()
-            #only send this hour news
-            if now.strftime('%H') == entity.strftime('%H'):
-                token = os.environ.get('BOT_TOKEN')
-                news = f'''
-                    <b>{head}</b>
+            image = story.find('img').get('src')
+            time = now.strftime('%b %d %Y %H:%M:%S')
+            news = f'''<b>{head}</b>
                     <pre>{body}</pre>
-                '''
-                requests.get(f'https://api.telegram.org/{token}/sendMessage?chat_id=-647851516&text={news}&parse_mode=HTML')
+                    <code>{time}</code>
+                    '''
+            token = environ.get('BOT_TOKEN')
+            chat_id = environ.get('CHAT_ID')
+            url = f'https://api.telegram.org/{token}/sendPhoto?chat_id=-{chat_id}&photo={image}&caption={news}&parse_mode=html'
+            requests.get(url)
